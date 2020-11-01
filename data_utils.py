7980d8a7841cc3792a824f7a2ce1194d4eb8663c
NATALIE LOKE YUN SUM <nataliel@xgpf0.comp.nus.edu.sg>
2020-11-01 15:57:25 +0800

    remove f0 and replace speaker with one-hot emotion

--- data_utils.py
+++ data_utils.py
@@ -14,11 +14,11 @@ from yin import compute_yin
 
 class TextMelLoader(torch.utils.data.Dataset):
     """
-        1) loads audio, text and speaker ids
+        1) loads audio, text and emotion ids
         2) normalizes text and converts them to sequences of one-hot vectors
-        3) computes mel-spectrograms and f0s from audio files.
+        3) computes mel-spectrograms from audio files.
     """
-    def __init__(self, audiopaths_and_text, hparams, speaker_ids=None):
+    def __init__(self, audiopaths_and_text, hparams, emotion_ids=None):
         self.audiopaths_and_text = load_filepaths_and_text(audiopaths_and_text)
         self.text_cleaners = hparams.text_cleaners
         self.max_wav_value = hparams.max_wav_value
@@ -30,8 +30,6 @@ class TextMelLoader(torch.utils.data.Dataset):
         self.sampling_rate = hparams.sampling_rate
         self.filter_length = hparams.filter_length
         self.hop_length = hparams.hop_length
-        self.f0_min = hparams.f0_min
-        self.f0_max = hparams.f0_max
         self.harm_thresh = hparams.harm_thresh
         self.p_arpabet = hparams.p_arpabet
 
@@ -39,40 +37,29 @@ class TextMelLoader(torch.utils.data.Dataset):
         if hparams.cmudict_path is not None:
             self.cmudict = cmudict.CMUDict(hparams.cmudict_path)
 
-        self.speaker_ids = speaker_ids
-        if speaker_ids is None:
-            self.speaker_ids = self.create_speaker_lookup_table(self.audiopaths_and_text)
+        self.emotion_ids = emotion_ids
+        if emotion_ids is None:
+            self.emotion_ids = self.create_emotion_lookup_table(self.audiopaths_and_text)
 
         random.seed(1234)
         random.shuffle(self.audiopaths_and_text)
 
-    def create_speaker_lookup_table(self, audiopaths_and_text):
-        speaker_ids = np.sort(np.unique([x[2] for x in audiopaths_and_text]))
-        d = {int(speaker_ids[i]): i for i in range(len(speaker_ids))}
+    def create_emotion_lookup_table(self, audiopaths_and_text):
+        emotion_ids = np.sort(np.unique([x[2] for x in audiopaths_and_text]))
+        d = {int(emotion_ids[i]): i for i in range(len(emotion_ids))}
         return d
 
-    def get_f0(self, audio, sampling_rate=22050, frame_length=1024,
-               hop_length=256, f0_min=100, f0_max=300, harm_thresh=0.1):
-        f0, harmonic_rates, argmins, times = compute_yin(
-            audio, sampling_rate, frame_length, hop_length, f0_min, f0_max,
-            harm_thresh)
-        pad = int((frame_length / hop_length) / 2)
-        f0 = [0.0] * pad + f0 + [0.0] * pad
-
-        f0 = np.array(f0, dtype=np.float32)
-        return f0
-
     def get_data(self, audiopath_and_text):
-        audiopath, text, speaker = audiopath_and_text
+        audiopath, text, emotion = audiopath_and_text
         text = self.get_text(text)
-        mel, f0 = self.get_mel_and_f0(audiopath)
-        speaker_id = self.get_speaker_id(speaker)
-        return (text, mel, speaker_id, f0)
+        mel = self.get_mel(audiopath)
+        emotion_id = self.get_emotion_id(emotion)
+        return (text, mel, emotion_id)
 
-    def get_speaker_id(self, speaker_id):
-        return torch.IntTensor([self.speaker_ids[int(speaker_id)]])
+    def get_emotion_id(self, emotion_id):
+        return torch.IntTensor([self.emotion_ids[int(emotion_id)]])
 
-    def get_mel_and_f0(self, filepath):
+    def get_mel(self, filepath):
         audio, sampling_rate = load_wav_to_torch(filepath)
         if sampling_rate != self.stft.sampling_rate:
             raise ValueError("{} SR doesn't match target {} SR".format(
@@ -82,13 +69,7 @@ class TextMelLoader(torch.utils.data.Dataset):
         melspec = self.stft.mel_spectrogram(audio_norm)
         melspec = torch.squeeze(melspec, 0)
 
-        f0 = self.get_f0(audio.cpu().numpy(), self.sampling_rate,
-                         self.filter_length, self.hop_length, self.f0_min,
-                         self.f0_max, self.harm_thresh)
-        f0 = torch.from_numpy(f0)[None]
-        f0 = f0[:, :melspec.size(1)]
-
-        return melspec, f0
+        return melspec
 
     def get_text(self, text):
         text_norm = torch.IntTensor(
@@ -134,26 +115,22 @@ class TextMelCollate():
             max_target_len += self.n_frames_per_step - max_target_len % self.n_frames_per_step
             assert max_target_len % self.n_frames_per_step == 0
 
-        # include mel padded, gate padded and speaker ids
+        # include mel padded, gate padded and emotion ids
         mel_padded = torch.FloatTensor(len(batch), num_mels, max_target_len)
         mel_padded.zero_()
         gate_padded = torch.FloatTensor(len(batch), max_target_len)
         gate_padded.zero_()
         output_lengths = torch.LongTensor(len(batch))
-        speaker_ids = torch.LongTensor(len(batch))
-        f0_padded = torch.FloatTensor(len(batch), 1, max_target_len)
-        f0_padded.zero_()
+        emotion_ids = torch.LongTensor(len(batch))
 
         for i in range(len(ids_sorted_decreasing)):
             mel = batch[ids_sorted_decreasing[i]][1]
             mel_padded[i, :, :mel.size(1)] = mel
             gate_padded[i, mel.size(1)-1:] = 1
             output_lengths[i] = mel.size(1)
-            speaker_ids[i] = batch[ids_sorted_decreasing[i]][2]
-            f0 = batch[ids_sorted_decreasing[i]][3]
-            f0_padded[i, :, :f0.size(1)] = f0
+            emotion_ids[i] = batch[ids_sorted_decreasing[i]][2]
 
         model_inputs = (text_padded, input_lengths, mel_padded, gate_padded,
-                        output_lengths, speaker_ids, f0_padded)
+                        output_lengths, emotion_ids)
 
         return model_inputs
